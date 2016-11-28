7502d81e96f00a7d3ed1025c7c2bd27d265a299f
imran hossain <imran096@gmail.com>
2016-11-28 20:43:14 +0600

    fixed app title and create favicon icon

--- public/services/ombd.service.js
+++ public/services/ombd.service.js
@@ -41,3 +41,6 @@
 			}
 			
 }())
+
+
+// "https://www.myapifilms.com/imdb/idIMDB?title=avatar&token=964c1664-abf7-4e67-91aa-4f0254da22aa&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=2&exactFilter=0&limit=1&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=1&biography=0&uniqueName=0&filmography=0&bornAndDead=0&starSign=0&actorActress=0&actorTrivia=0&similarMovies=0&adultSearch=0&goofs=0&keyword=0&quotes=0&fullSize=0&companyCredits=0&filmingLocations=1"
